a923ae94b665c9ea966240104b73b8f4201ab714
Naowal Rahman <naowal.ar@gmail.com>
2021-05-15 11:46:28 -0400

    added segment tree

--- cs-article-snippets/quick-sort.py
+++ cs-article-snippets/quick-sort.py
@@ -2,18 +2,18 @@ def partition(arr, low, high):
     i = low - 1
     pivot = arr[high]
 
-    for val in range(low, high): 
+    for val in range(low, high):
         if arr[val] < pivot:
             i += 1 # set the index value
             arr[val], arr[i] = arr[i], arr[val]
-    
+
     # swap the high value with the value higher
     arr[i + 1], arr[high] = arr[high], arr[i + 1]
 
     return i + 1
 
 def quicksort(arr, low, high):
-    if low < high: 
+    if low < high:
         # set the partition index
         pi = partition(arr, low, high)
 
@@ -24,4 +24,4 @@ def quicksort(arr, low, high):
 
 array = [11, 9, 29, 7, 2, 15, 28]
 quicksort(array, 0, len(array) - 1)
-print(array) 
+print(array)
