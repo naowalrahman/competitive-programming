8cf8e201b8e2b14728da5e497b75ab668ddb1c34
Naowal Rahman <naowal.ar@gmail.com>
2022-01-25 21:44:55 -0500

    cpp & base5 recursion

---
+++ codeforces/339A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string str;
+    getline(cin, str);
+
+    vector<char> v;
+
+    for(char c: str)
+        if(c != '+') 
+            v.push_back(c);
+        
+
+
+    sort(v.begin(), v.end()); 
+
+    for(int i = 0; i < v.size(); i++)
+        if(i != v.size() - 1) 
+            cout << v[i] << '+';
+        else cout << v[i];
+    
+}
