c62474101892f10ffd2f6254e06e6fbaf4fe6506
Naowal Rahman <naowal.ar@gmail.com>
2022-04-06 14:08:02 -0400

    atcoder abc230 problem A

---
+++ atcoder/A-Atcoder_Quiz_3.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n; cin >> n;
+    cout << "AGC0";
+    if(n < 42) cout << n;
+    else cout << n + 1;   
+
+    return 0;
+}
