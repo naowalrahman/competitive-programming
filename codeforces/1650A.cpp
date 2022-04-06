0dfe9f393425498235841bc70b4542e820ea12f4
Naowal Rahman <naowal.ar@gmail.com>
2022-04-06 13:35:04 -0400

    4 old codeforces problems

---
+++ codeforces/1650A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t; cin >> t;
+    for(int i = 0, i < t; i++) {
+        // s.length() is odd
+        string s; cin >> s;
+        string c; cin >> c;
+        
+    }
+    return 0;
+}
