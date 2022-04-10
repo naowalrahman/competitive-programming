e234991a62bb049e253479c49aa1626a4fa1c90b
Naowal Rahman <naowal.ar@gmail.com>
2022-04-10 14:15:17 -0400

    atcoder abc247 D & cf 1660A

--- atcoder-p/D-Cylinder.cpp
+++ atcoder-p/D-Cylinder.cpp
@@ -1,7 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
 
 int main() {
-    long long sum = 0;
-    
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    ll q; cin >> q;
+    queue<pair<ll, ll> > a;
+
+    for(ll i = 0; i < q; i++) {
+        ll d; cin >> d;
+        
+        if(d == 1) {
+            ll x, c; cin >> x >> c;
+            // count is c, actual num is x
+            a.push(pair<ll, ll>(x, c));
+        }
+        else {
+            ll c; cin >> c;
+            ll sum = 0;
+            while(c) {
+                if(a.front().second == 0) {
+                    a.pop();
+                    continue;
+                }
+                ll mn = min(c, a.front().second);
+                c -= mn;
+                a.front().second -= mn;
+
+                sum += a.front().first * mn;
+            }
+            cout << sum << "\n";
+        }
+
+    }    
 }
