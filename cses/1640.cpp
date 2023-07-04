597f5c41e415f4062e6caa57bc8956b46d5434b5
Naowal Rahman <naowal.ar@gmail.com>
2023-07-04 13:05:28 -0400

    cses 1641

--- cses/1640.cpp
+++ cses/1640.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 
 int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    
     int n, target; cin >> n >> target;
     vector<pair<int, int>> a(n);
     for(int i = 0; i < n; i++) {
