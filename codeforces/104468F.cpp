d96d46ee8d827461a8e243b83a5f5902457b1355
Naowal Rahman <naowal.ar@gmail.com>
2023-07-23 18:20:31 -0400

    ccc potd & cf 1853B

---
+++ codeforces/104468F.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    int t, n, a, b; cin >> t;
+    while(t--) {
+        cin >> n;
+        a = n + 1;
+        b = 2;
+        cout << a << " " << b << "\n";
+    }    
+    
+    return 0;
+}
