d4bd48855ea1cfad99f069e058fb3e31ac2ea451
Naowal Rahman <naowal.ar@gmail.com>
2023-12-14 15:07:11 -0500

    Update counting_haybales.cpp

--- usaco/silver/counting_haybales.cpp
+++ usaco/silver/counting_haybales.cpp
@@ -18,9 +18,7 @@ int main() {
 		cin >> a >> b;
 		auto itr_a = lower_bound(all(arr), a);
 		auto itr_b = prev(upper_bound(all(arr), b));
-
-		if(itr_a == arr.end() || itr_b == arr.end()) cout << 0 << "\n";
-		else cout << itr_b - itr_a + 1<< "\n";
+		cout << itr_b - itr_a + 1<< "\n";
 	}
 
 	return 0;
