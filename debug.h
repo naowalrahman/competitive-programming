9d8fe85d1857445ca6bbe362fe0877130bfb19ff
Naowal Rahman <naowal.ar@gmail.com>
2024-01-07 12:14:45 -0500

    cpp template

---
+++ debug.h
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int recur_depth = 0;
+#define dbg(x) {++recur_depth; auto x_=x; --recur_depth; cerr<<string(recur_depth, '\t')<<"\e[91m"<<__func__<<":"<<__LINE__<<"\t"<<#x<<" = "<<x_<<"\e[39m"<<"\n";}
+
+template<typename Ostream, typename Cont>
+typename enable_if<is_same<Ostream,ostream>::value, Ostream&>::type operator<<(Ostream& os,  const Cont& v){
+	os<<"[";
+	for(auto& x:v){os<<x<<", ";}
+	return os<<"]";
+}
+template<typename Ostream, typename ...Ts>
+Ostream& operator<<(Ostream& os,  const pair<Ts...>& p){
+	return os<<"{"<<p.first<<", "<<p.second<<"}";
+}
---
+++ template.cpp
@@ -0,0 +1,47 @@
+/**
+ * @file template.cpp
+ * @author Naowal Rahman
+ * @date 2024-01-05 14:27
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#include "/home/naowal/Desktop/code/competitive-programming/debug.h"
+#else
+#define dbg(x)
+#endif
+
+#define int long long
+template<class T> using V = vector<T>;
+using vi = V<int>;
+using pii = pair<int, int>;
+#define sz(x) (int)((x).size())
+#define all(x) begin(x), end(x)
+#define rall(x) rbegin(x), rend(x)
+
+#define FOR(i,a,b) for (int i = (a); i < (b); ++i)
+#define F0R(i,a) FOR(i,0,a)
+#define ROF(i,a,b) for (int i = (b)-1; i >= (a); --i)
+#define R0F(i,a) ROF(i,0,a)
+
+template<class T> bool ckmin(T& a, const T& b) {
+	return b < a ? a = b, 1 : 0; } // set a = min(a,b)
+template<class T>  bool ckmax(T& a, const T& b) {
+	return a < b ? a = b, 1 : 0; } // set a = max(a,b)
+
+signed main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    return 0;
+}
+
+/* stuff you should look for
+   -------------------------
+ * int overflow, array bounds
+ * special cases (n=1?)
+ * do smth instead of nothing and stay organized
+ * WRITE STUFF DOWN
+ * DON'T GET STUCK ON ONE APPROACH
+*/
