98476d82597b871184815bdb1eebe9e0f5574629
Naowal Rahman <naowal.ar@gmail.com>
2024-02-24 19:31:15 -0500

    cf 1923B

---
+++ codeforces/1923B.cpp
@@ -0,0 +1,69 @@
+/**
+ * @file 1923B.cpp
+ * @author Naowal Rahman
+ * @date 2024-02-24 19:10
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#include "/home/naowal/Desktop/code/competitive-programming/debug.h"
+#else
+#define dbg(x)
+#endif
+
+#define int long long
+template<class T> using V = vector<T>;
+using vi = V<int>;
+using pii = pair<int, int>;
+#define sz(x) (int)((x).size())
+#define all(x) begin(x), end(x)
+#define rall(x) rbegin(x), rend(x)
+
+#define FOR(i,a,b) for (int i = (a); i < (b); ++i)
+#define F0R(i,a) FOR(i,0,a)
+#define ROF(i,a,b) for (int i = (b)-1; i >= (a); --i)
+#define R0F(i,a) ROF(i,0,a)
+
+template<class T> bool ckmin(T& a, const T& b) {
+    return b < a ? a = b, 1 : 0; } // set a = min(a,b)
+template<class T>  bool ckmax(T& a, const T& b) {
+    return a < b ? a = b, 1 : 0; } // set a = max(a,b)
+
+void solve() {
+    int n, k; cin >> n >> k;
+    V<pii> m(n);
+    for(auto &p : m) cin >> p.second;
+    for(auto &p : m) {
+        cin >> p.first;
+        p.first = abs(p.first);
+    }
+    sort(all(m));
+
+    int rem = 0, i = 0, dist = 1;
+    while(i < n) {
+        int shots = k + rem;
+        while(i < n && m[i].first == dist) {
+            shots -= m[i].second;
+            if(shots < 0) {
+                cout << "NO\n";
+                return;
+            }
+            ++i;
+        }
+        rem = shots;
+        ++dist;
+    }
+
+    cout << "YES\n";
+}
+
+signed main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    int t; cin >> t;
+    while(t--) solve();
+
+    return 0;
+}
